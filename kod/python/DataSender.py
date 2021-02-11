e5214e7655f7a09f7740378286d5ff766fedf0b5
Karl Johannes <kj@jondell.com>
2021-02-11 09:57:03 +0100

    stashing

--- kod/python/DataSender.py
+++ kod/python/DataSender.py
@@ -1,7 +1,7 @@
 from cgmparser.Parser import Parser
 from pathlib import Path
 from pythonosc import udp_client
-from Calculator import Calculator
+
 import numpy
 import io
 import time
@@ -21,39 +21,12 @@ class DataSender:
         else:
             return
 
-        # client.send_message('/newPackage', '') # implementera start-flagg
+        self.client.send_message('/newPackage', '') # implementera start-flagg
         for value in values:
             self.client.send_message('/value', value) # test som skickar OSC meddelande till Supercollider (Sched.scd)
+        self.client.send_message('/meta', ["mean", numpy.mean(values)])
         self.client.send_message('/valueDone', '') # test som skickar OSC meddelande till Supercollider (Sched.scd)
 
-# times, values = parser().parse_csv_data(filename="Karl JohannesJondell_glucose_7-12-2020.csv")
-# values = Calculator.get_differentiated(values, 0.5) # step_size = 1, to not scale data...
-# #zipped_lists = zip(times, values)
-# #sorted_list = sorted(zipped_lists)
-# 
 # print(numpy.mean(values), numpy.median(values), numpy.max(values), numpy.min(values))
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--ip", default="127.0.0.1")
-# parser.add_argument("--port", default=7771)
-# args = parser.parse_args()
-# client = udp_client.SimpleUDPClient(args.ip, args.port)
-# 
-# client.send_message("/newPacket", "differentiated")
-# 
-# def send_osc_message(value, time_value=None):
-#     client.send_message("/value", value)
-# 
-#     if time_value != None:
-#         client.send_message("/time", time_value)
-#     
-# for value in values:
-#     send_osc_message(value)
-# 
-# # for time_value, value in sorted_list:
-# #     send_osc_message(time_value, value)
-# # 
-# # # skicka medelvärde (mean, mode, median), max o min, 
-# # client.send_message("/meta", [numpy.mean(values)])
-# # 
-# client.send_message("/done", "")
-# 
+# # skicka medelvärde (mean, mode, median), max o min, 
+# client.send_message("/meta", [numpy.mean(values)])
--- kod/radio/readme.md
+++ kod/radio/readme.md
@@ -1,5 +1,9 @@
 # Radio inställningar
 
+## Jack
+
+Starta jack m.h.a `jackd -R -n scjack -d dummy -C 0 -P 0 -p 256`
+
 ## Icecast
 
 Icecast (eller mer specifikt, *Icecast2*) körs som en service på server-maskinen (Ubuntu), och kontrolleras därför med `systemctl` (dvs. `sudo systemctl status icecast2` för status, `reboot` och dylikt för kontroll). Ändra i /etc/icecast2/icecast.xml för att t.ex. byta port, ändra antalet uppkopplade klienter, källor, http-headers, och dylikt.
